d94d491ae88fbc52f4ce7c8632e4c804345ce2ea
hbao448 <hbao448@aucklanduni.ac.nz>
2016-09-06 17:15:32 +1200

    Add files via upload

---
+++ VideoPlayer.java
@@ -0,0 +1,123 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+import uk.co.caprica.vlcj.runtime.RuntimeUtil;
+
+
+public class VideoPlayer {
+	
+    private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
+	private Quiz _quiz;
+
+    public VideoPlayer(Quiz quiz) {
+    	
+    	_quiz = quiz;
+    
+        JFrame frame = new JFrame("The Awesome Mediaplayer");
+
+        mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+
+        final EmbeddedMediaPlayer video = mediaPlayerComponent.getMediaPlayer();
+        
+        frame.add(mediaPlayerComponent);
+
+        JButton pause = new JButton("Pause");
+        JButton stop = new JButton("Stop");
+        JProgressBar progress = new JProgressBar();
+        
+        JPanel pauseStop = new JPanel();
+        
+        pauseStop.add(pause, JPanel.LEFT_ALIGNMENT);
+        pauseStop.add(stop, JPanel.RIGHT_ALIGNMENT);
+        
+        JPanel menu = new JPanel();
+        
+        menu.setLayout(new GridLayout(2,1));
+        
+        menu.add(progress);
+        menu.add(pauseStop);
+        
+        frame.add(menu, BorderLayout.SOUTH);
+        
+        pause.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (pause.getText().equals("Pause")) {
+					video.setPause(true);
+					pause.setText("Resume");
+				} else {
+					video.setPause(false);
+					pause.setText("Pause");
+				}
+			}
+        	
+        });
+        
+        stop.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				video.stop();
+				frame.dispose();
+				_quiz.frame.setVisible(true);
+			}
+        	
+        });
+        
+        Timer timer = new Timer(50, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				long time = video.getTime();
+				long totalTime = video.getLength();
+				if (totalTime != 0) {
+				if (time == totalTime) {
+					pause.setEnabled(false);
+				}
+				progress.setValue((int) ((time*100)/totalTime));
+				}
+			}
+		});
+        
+        frame.setLocation(100, 100);
+        frame.setSize(1050, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        //String filename = "big_buck_bunny_1_minute.avi";
+        String filename = "out.avi";
+        video.playMedia(filename);
+        timer.start();
+    }
+
+    /*public static void main(final String[] args) {
+        
+        NativeLibrary.addSearchPath(
+            RuntimeUtil.getLibVlcLibraryName(), "/Applications/vlc-2.0.0/VLC.app/Contents/MacOS/lib"
+        );
+        Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                //new VideoPlayer(args);
+            }
+        });
+    }*/
+}
