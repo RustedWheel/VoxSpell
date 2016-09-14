e22b8817dd2f1f9a4fe769c2e92d0cd8d696bdb2
hbao448 <hbao448@aucklanduni.ac.nz>
2016-09-14 18:43:12 +1200

    Add files via upload

--- Spelling_Aid.java
+++ Spelling_Aid.java
@@ -7,8 +7,10 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -19,9 +21,10 @@ import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JComboBox;
 
 @SuppressWarnings("serial")
-public class Spelling_Aid extends JFrame{
+public class Spelling_Aid extends JFrame {
 
 	private JButton quiz = new JButton("New Spelling Quiz");
 	private JButton review = new JButton("Review Mistakes");
@@ -30,6 +33,13 @@ public class Spelling_Aid extends JFrame{
 	private JTextArea txtOutput = new JTextArea(10, 20);
 	private Quiz _quiz;
 	private Statistics _statistics;
+	private String[] levels = { "Level 1", "Level 2", "Level 3", "Level 4", "Level 5", "Level 6", "Level 7", "Level 8",
+			"Level 9", "Level 10", "Level 11" };
+	private JComboBox selectLV = new JComboBox(levels);
+	private JComboBox selectVoices;
+	private int _level = 1;
+	private ArrayList<String> _availableVoices = new ArrayList<String>();
+	private String _voicePath = "/usr/share/festival/voices";
 
 	public static void main(String[] Args) {
 		SwingUtilities.invokeLater(new Runnable() {
@@ -50,9 +60,11 @@ public class Spelling_Aid extends JFrame{
 	}
 
 	/**
-	 * This method turns a list of words to be spoken into speech by calling festival
-	 * using bash
-	 * @param texts The list of strings to be spoken in order
+	 * This method turns a list of words to be spoken into speech by calling
+	 * festival using bash
+	 * 
+	 * @param texts
+	 *            The list of strings to be spoken in order
 	 */
 	public void textToSpeech(ArrayList<String> texts) {
 
@@ -61,6 +73,7 @@ public class Spelling_Aid extends JFrame{
 		// Creates a list of commands for the SwingWorker to execute
 		for (String text : texts) {
 			commands.add("echo " + "\"" + text + "\" | festival --tts");
+			System.out.println(text);
 		}
 		// Creates a new SwingWorker and executes the commands
 		Speaker worker = new Speaker(commands);
@@ -69,8 +82,11 @@ public class Spelling_Aid extends JFrame{
 	}
 
 	/**
-	 * This method reads out a word and then the letters of that word individually 
-	 * @param word the word to split into individual characters and read out
+	 * This method reads out a word and then the letters of that word
+	 * individually
+	 * 
+	 * @param word
+	 *            the word to split into individual characters and read out
 	 */
 	public void spellOut(String word) {
 
@@ -90,7 +106,9 @@ public class Spelling_Aid extends JFrame{
 
 	/**
 	 * This method executes a single bash command through the use of a Process
-	 * @param command The command to be executed using bash
+	 * 
+	 * @param command
+	 *            The command to be executed using bash
 	 */
 	public static void bashCommand(String command) {
 
@@ -105,16 +123,18 @@ public class Spelling_Aid extends JFrame{
 	}
 
 	/**
-	 * This method reads each line in a file and stores each line into
-	 * an ArrayList of strings and returns the ArrayList
-	 * @param file The name of the file to be read
+	 * This method reads each line in a file and stores each line into an
+	 * ArrayList of strings and returns the ArrayList
+	 * 
+	 * @param file
+	 *            The name of the file to be read
 	 * @return An ArrayList containing the lines of a file in sequential order
 	 */
 	public ArrayList<String> readList(File file) {
 
 		ArrayList<String> results = new ArrayList<String>();
 
-		try{
+		try {
 
 			FileReader fr = new FileReader(file);
 			BufferedReader br = new BufferedReader(fr);
@@ -134,20 +154,72 @@ public class Spelling_Aid extends JFrame{
 
 	}
 
+	public ArrayList<String> readLevel(File file, int level) {
+
+		ArrayList<String> results = new ArrayList<String>();
+
+		int next = level + 1;
+
+		String levelString = "%Level " + level;
+		String nextLevel = "%Level " + next;
+
+		try {
+
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+
+			String line;
+			while ((line = br.readLine()) != null && !line.equals(levelString)) {
+			}
+
+			line = br.readLine();
+
+			while (line != null) {
+				if (line.equals(nextLevel)) {
+					break;
+				}
+				results.add(line);
+				line = br.readLine();
+			}
+
+			br.close();
+			fr.close();
+
+		} catch (IOException e1) {
+
+		}
+
+		return results;
+
+	}
+
 	public Spelling_Aid() {
 		super("Spelling Aid");
 		setSize(400, 400);
-		GridLayout layout = new GridLayout(2,2);
-		
+		GridLayout layout = new GridLayout(2, 2);
+
 		JPanel menu = new JPanel();
 		menu.setLayout(layout);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		selectLV.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				JComboBox lv = (JComboBox) evt.getSource();
+				String selectedlv = (String) lv.getSelectedItem();
+				String[] level = selectedlv.split(" ");
+				_level = Integer.parseInt(level[1]);
+			}
+		});
+
 		quiz.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+
+				JOptionPane.showMessageDialog(null, selectLV, "Please select a level", JOptionPane.QUESTION_MESSAGE);
+
 				// Starts a new quiz and hides the main menu
-				_quiz = new Quiz(Quiz.quizType.QUIZ, Spelling_Aid.this);
+				_quiz = new Quiz(Quiz.quizType.QUIZ, Spelling_Aid.this, _level);
 				setVisible(false);
 				_quiz.startQuiz();
 			}
@@ -158,7 +230,7 @@ public class Spelling_Aid extends JFrame{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// Starts a new review and hides the main menu
-				_quiz = new Quiz(Quiz.quizType.REVIEW, Spelling_Aid.this);
+				_quiz = new Quiz(Quiz.quizType.REVIEW, Spelling_Aid.this, 0);
 				setVisible(false);
 				_quiz.startQuiz();
 
@@ -181,11 +253,14 @@ public class Spelling_Aid extends JFrame{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				// Prompts the user if they are sure that they want to clear their statistics
-				int choice = JOptionPane.showConfirmDialog(null, "Are you sure you wish to clear all statistics?", "Clear statistics", JOptionPane.YES_NO_OPTION);
+				// Prompts the user if they are sure that they want to clear
+				// their statistics
+				int choice = JOptionPane.showConfirmDialog(null, "Are you sure you wish to clear all statistics?",
+						"Clear statistics", JOptionPane.YES_NO_OPTION);
 
 				if (choice == JOptionPane.YES_OPTION) {
-					// If they choose yes then their statistics are cleared and a message is displayed telling them so
+					// If they choose yes then their statistics are cleared and
+					// a message is displayed telling them so
 					clearStatistics();
 
 					JOptionPane.showMessageDialog(new JFrame(), "Successfully cleared statistics", "Cleared Statistics",
@@ -199,17 +274,35 @@ public class Spelling_Aid extends JFrame{
 			}
 
 		});
+		
+		for(String directory : listDirectories(_voicePath)){
+			String[] subDirectories = listDirectories(_voicePath + "/" + directory);
+			for(String voices : subDirectories){
+				_availableVoices.add(voices);
+			}
+		}
+		
+		selectVoices = new JComboBox(_availableVoices.toArray());
+		
+		selectVoices.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				JComboBox voice = (JComboBox) evt.getSource();
+				String selectedvoice = (String) voice.getSelectedItem();
+				setVoice(selectedvoice);
+			}
+		});
 
-		txtOutput.setText("Welcome to the Spelling Aid!\n\nPress \"New Quiz\" to start a new quiz.\nPress \"Review\" to review previously failed words\nPress \"View Statistics\" to view your current statistics\nPress \"Clear Statistics\" to clear all current statistics");
+		txtOutput.setText(
+				"Welcome to the Spelling Aid!\n\nPress \"New Quiz\" to start a new quiz.\nPress \"Review\" to review previously failed words\nPress \"View Statistics\" to view your current statistics\nPress \"Clear Statistics\" to clear all current statistics");
 		txtOutput.setEditable(false);
 
 		menu.add(quiz);
 		menu.add(review);
 		menu.add(statistics);
 		menu.add(clear);
+		menu.add(selectVoices);
 
 		add(txtOutput, BorderLayout.NORTH);
-
 		add(menu);
 
 		setResizable(false);
@@ -218,11 +311,10 @@ public class Spelling_Aid extends JFrame{
 	}
 
 	/**
-	 * This method deletes the existing results and failed files and then creates
-	 * new ones that are empty
+	 * This method deletes the existing results and failed files and then
+	 * creates new ones that are empty
 	 */
 	protected void clearStatistics() {
-
 
 		File file = new File(".results");
 
@@ -242,16 +334,21 @@ public class Spelling_Aid extends JFrame{
 
 		}
 
-
 	}
 
 	/**
-	 * This method appends a word and the grade for that word to the results file
-	 * @param currentWord The word to append
-	 * @param attempts How many attempts the user had on the currentWord
-	 * @param correct A boolean value representing if the user correctly spelled the word
+	 * This method appends a word and the grade for that word to the results
+	 * file
+	 * 
+	 * @param currentWord
+	 *            The word to append
+	 * @param attempts
+	 *            How many attempts the user had on the currentWord
+	 * @param correct
+	 *            A boolean value representing if the user correctly spelled the
+	 *            word
 	 */
-	public void appendList(String currentWord, int attempts, boolean correct) {
+	public void appendList(int level, int score) {
 
 		BufferedWriter bw = null;
 
@@ -259,21 +356,19 @@ public class Spelling_Aid extends JFrame{
 			// Opens the .results file for appending
 			bw = new BufferedWriter(new FileWriter(".results", true));
 
-			// If they answered correctly in 1 attempt then that word is mastered
-			if (attempts == 1) {
-				bw.write(currentWord + " mastered");
-				bw.newLine();
-				// If they answered correctly in 2 attempts then that word is faulted
-			} else if (attempts == 2 && correct) {
-				bw.write(currentWord + " faulted");
-				bw.newLine();
-				// If they failed both attempts then that word is failed, and also added
-				// to the failed list
-			} else {
-				bw.write(currentWord + " failed");
-				bw.newLine();
-				appendFailed(currentWord);
-			}
+			/*
+			 * // If they answered correctly in 1 attempt then that word is
+			 * mastered if (attempts == 1) { bw.write(currentWord + " mastered"
+			 * ); bw.newLine(); // If they answered correctly in 2 attempts then
+			 * that word is faulted } else if (attempts == 2 && correct) {
+			 * bw.write(currentWord + " faulted"); bw.newLine(); // If they
+			 * failed both attempts then that word is failed, and also added //
+			 * to the failed list } else { bw.write(currentWord + " failed");
+			 * bw.newLine(); appendFailed(currentWord); }
+			 */
+
+			bw.write("Level" + level + " " + score);
+			bw.newLine();
 
 		} catch (IOException e) {
 
@@ -285,23 +380,25 @@ public class Spelling_Aid extends JFrame{
 			}
 		}
 
-
 	}
 
 	/**
 	 * This method appends a word to the failed file if the word is not already
 	 * on the failed file
-	 * @param currentWord The word to append to the failed file
+	 * 
+	 * @param currentWord
+	 *            The word to append to the failed file
 	 */
-	private void appendFailed(String currentWord) {
-
+	public void appendFailed(String currentWord, int level) {
 		ArrayList<String> failed = readList(new File(".failed"));
 
-		// If the failed list does not contain the word to be added, then it is added
+		// If the failed list does not contain the word to be added, then it is
+		// added
 		if (!failed.contains(currentWord)) {
 
 			BufferedWriter bw = null;
 
+			currentWord = currentWord + "	" + level;
 			try {
 				bw = new BufferedWriter(new FileWriter(".failed", true));
 				bw.write(currentWord);
@@ -313,13 +410,14 @@ public class Spelling_Aid extends JFrame{
 
 		}
 
-
 	}
 
 	/**
-	 * This method removes a word from the failed list if it exists there, 
-	 * it is intended to be called once the user correctly spells a word 
-	 * @param currentWord The word to remove from the failed file
+	 * This method removes a word from the failed list if it exists there, it is
+	 * intended to be called once the user correctly spells a word
+	 * 
+	 * @param currentWord
+	 *            The word to remove from the failed file
 	 */
 	public void removeWord(String currentWord) {
 
@@ -342,17 +440,16 @@ public class Spelling_Aid extends JFrame{
 
 		}
 
-
-
 	}
 
 	/**
-	 * This is a private SwingWorker class that executes a list of bash commands sequentially
-	 * on a worker thread
+	 * This is a private SwingWorker class that executes a list of bash commands
+	 * sequentially on a worker thread
+	 * 
 	 * @author Hunter
 	 *
 	 */
-	class Speaker extends SwingWorker<Void,Void> {
+	class Speaker extends SwingWorker<Void, Void> {
 
 		private ArrayList<String> _commands;
 
@@ -378,5 +475,23 @@ public class Spelling_Aid extends JFrame{
 			}
 		}
 
+	}
+	
+	public String[] listDirectories(String directory){
+		
+		File file = new File(directory);
+		String[] subDirectories = file.list(new FilenameFilter() {
+		  @Override
+		  public boolean accept(File current, String name) {
+		    return new File(current, name).isDirectory();
+		  }
+		});
+		
+		return subDirectories;
+	}
+	
+	public void setVoice(String voice){
+		String command = "echo " + "\"(set! voice_default '" + voice + ")\""  + "> ~/.festivalrc";
+		bashCommand(command);
 	}
 }
